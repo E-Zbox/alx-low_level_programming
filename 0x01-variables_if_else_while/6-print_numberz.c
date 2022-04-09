54f8a19b4a2ba356e4dc4e7e3b2ef7818491771f
E-Zbox <zboxstudios97@gmail.com>
2022-04-08 17:54:55 -0700

    Correct output

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Print all single digit numbers using only putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+		putchar(digit + '0');
+
+	putchar('\n');
+
+	return (0);
+}
