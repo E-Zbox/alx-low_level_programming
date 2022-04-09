fa3a9a3f280487bdc99a5083157686c269fc9915
E-Zbox <zboxstudios97@gmail.com>
2022-04-08 18:27:38 -0700

    9. Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	int single_num;
 
-	for(single_num = 0; single_num < 10; single_num++)
+	for (single_num = 0; single_num < 10; single_num++)
 	{
 		putchar(single_num + '0');
 		if (single_num < 9)
