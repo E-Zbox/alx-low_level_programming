603a3441a39310fb7a643ee4f800eeac20fa6ead
E-Zbox <zboxstudios97@gmail.com>
2022-04-12 15:28:04 -0700

    2. 10x alphabet

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _islower - checks if character has lower case
+ *
+ * Return: Always 0||1 (Success)
+ */
+int _islower(int count)
+{
+	return (count >= 'a' && count <= 'z');
+}
