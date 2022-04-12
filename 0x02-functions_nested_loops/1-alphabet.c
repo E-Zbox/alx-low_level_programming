899a980cca55e2e75b8d050aa10296f33e0b0e26
E-Zbox <zboxstudios97@gmail.com>
2022-04-12 15:16:36 -0700

    1. I sometime suffer from insomia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print all alphabet
+ *
+ * Return: Always void (Success)
+ */
+void print_alphabet(void)
+{
+	char _char;
+
+	for (_char = 'a'; _char <= 'z'; _char++)
+	{
+		_putchar(_char);
+	}
+	
+	_putchar('\n');
+}
