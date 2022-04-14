913f8c276a06559e59e9bf0bb06876513ef18fb9
E-Zbox <zboxstudios97@gmail.com>
2022-04-14 15:52:17 +0000

    0.isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isupper: returns 1 if _char is uppercase otherwise, 0
+ * @_char: int
+ * Returns: Always int (Success)
+ */
+int _isupper(int _char)
+{
+	return ((_char >= 'A') && (_char <= 'Z'));
+}
