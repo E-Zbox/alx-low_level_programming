7410bf75bfca343dc227b2d19197c53222caa016
Ebenezer <zboxstudios97@gmail.com>
2022-04-15 04:20:49 +0100

    Updated main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,2 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+
 int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
+void convert_day(int month, int day);
+
+#endif
