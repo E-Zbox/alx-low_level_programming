70bd50c7a54b73b31a91b10676c75b710cba9b2a
E-Zbox <zboxstudios97@gmail.com>
2022-04-20 23:57:05 +0100

    betty style coding

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -5,12 +5,13 @@
 /**
  * _puts - prints a string, followed by a new line, to stdout
  * @str: char
- * 
+ *
  * Return: void
  */
 void _puts(char *str)
 {
 	int count, len;
+
 	len = strlen(str);
 
 	for (count = 0; count < len; count++)
