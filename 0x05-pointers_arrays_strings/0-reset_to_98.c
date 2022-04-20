0ec4f2a2cf98a44786a5dfd804bbfb4d7dff0c90
E-Zbox <zboxstudios97@gmail.com>
2022-04-20 23:01:53 +0100

    betty style documentation

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * reset_to_98 - takes a pointer to an int as parameter
+ * reset_to_98 - takes a pointer to an int as parameter 
  * and updates the value it points to to 98
- * @n - int pointer
+ * @n: int pointer
  *
  * Return: Always 0
  */
