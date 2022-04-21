37c8813cf988b8d40ba32f9347aac73e180249b5
E-Zbox <zboxstudios97@gmail.com>
2022-04-21 16:52:40 +0100

    6. Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts2 - prints every even character of a string,
+ * starting with the first character, followed by a new line
+ * @str: string literal
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int count = 0;
+
+	while (*str != '\0')
+	{
+		if (count % 2 == 0)
+			putchar(*str);
+
+		count++;
+		*str++;
+	}
+
+	putchar('\n');
+}
