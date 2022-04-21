65cf6d0daa0115f6f68ac16a3f5cccd7789fc589
E-Zbox <zboxstudios97@gmail.com>
2022-04-21 22:05:33 +0100

    compile program

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -17,11 +17,11 @@ void print_array(int *a, int n)
 	{
 		if (count == n - 1)
 		{
-			printf("%d\n", a);
+			printf("%d\n", *a);
 			break;
 		}
 
-		printf("%d, ", a);
+		printf("%d, ", *a);
 
 		count++;
 		a++;
