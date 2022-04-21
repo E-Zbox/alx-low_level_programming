efc1e66b81a17e93bb4a25c69b6cdc7e04b6a8b3
E-Zbox <zboxstudios97@gmail.com>
2022-04-21 08:16:57 +0100

    correct code

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,9 +10,13 @@
 void rev_string(char *s)
 {
 	int count, len;
-	char *string = *s;
+	char *string;
 
 	len = strlen(s);
+
+	/* create a copy of string 's' */
+	for (count = 0; count < len; count++)
+		string[count] = s[count]
 	for (count = len; count >= 0; count--)
 		s[len - count] = string[count];
 
