6f686a4724fc32ea00a314c49413c0b842379a2b
E-Zbox <zboxstudios97@gmail.com>
2022-04-25 00:31:46 +0100

    1. strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strncat - similar to the _strcat function, except that
+ * it will use at most n bytes from src; and
+ * src does not need to be null-terminated if it contains n or more bytes
+ * @dest: string literal
+ * @src: string literal
+ * @n: int
+ *
+ * Return: a pointer to the resulting string dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int count = 0;
+	int len_dest = strlen(dest);
+
+	for (; src[count] && count < n; count++)
+		dest[len_dest + count] = src[count];
+
+	dest[len_dest + count] = '\0';
+
+	return dest;
+}
