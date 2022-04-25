24acfe1a4be7f301db141e84f62238c6bc720385
E-Zbox <zboxstudios97@gmail.com>
2022-04-25 02:16:11 +0100

    6. Expect the best. Prepare for the worst. Capitalize on what comes

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @_str: string literal
+ *
+ * Return: string literal
+ */
+char *cap_string(char *_str)
+{
+	int count = 0;
+	int delta = 'a' - 'A';
+
+	for (; _str[count]; count++)
+	{
+		if ((_str[count-1] == ' ') && (_str[count] > 'a') && (_str[count] < 'z'))
+			_str[count] -= delta;
+	}
+
+	return _str;
+}
