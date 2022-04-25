87c1c6eeb9eca7a65af7808cafb1611eb2582af1
E-Zbox <zboxstudios97@gmail.com>
2022-04-25 17:10:12 +0100

    Correct output

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,12 +8,14 @@
  */
 char *cap_string(char *_str)
 {
-	int count = 0;
+	int count = 1;
 	int delta = 'a' - 'A';
+
+	_str[0] -= delta;
 
 	for (; _str[count]; count++)
 	{
-		if (((_str[count - 1] == ' ') || (_str[count - 1] == '\t') || (_str[count - 1] == '\n')) && ((_str[count] >= 'a') && (_str[count] <= 'z')))
+		if (((_str[count - 1] == ' ') || (_str[count - 1] == '\t') || (_str[count - 1] == '\n') || (_str[count - 1] == '.')) && ((_str[count] >= 'a') && (_str[count] <= 'z')))
 			_str[count] -= delta;
 	}
 
