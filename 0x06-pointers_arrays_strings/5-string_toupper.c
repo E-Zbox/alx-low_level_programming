3982640d0429c4e5932d6d0ef1b398ca7c498e27
E-Zbox <zboxstudios97@gmail.com>
2022-04-25 02:26:31 +0100

    5. Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * @_char: string literal
+ *
+ * Return: string literal
+ */
+char *string_toupper(char *_char)
+{
+	int count = 0;
+	int delta = 'a' - 'A';
+
+	for (; _char[count]; count++)
+	{
+		if ((_char[count] >= 'a') && (_char[count] <= 'z'))
+			_char[count] -= delta;
+	}
+
+	return _char;
+}
