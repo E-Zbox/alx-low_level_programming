c78fd1174f11841c55f189b963d4909c6f5aa7ab
E-Zbox <zboxstudios97@gmail.com>
2022-04-25 13:28:26 +0100

    updated main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_FILE
+#define MAIN_FILE
+
 char *_strcat(char *dest, char *src);
 
 char *_strncat(char *dest, char *src, int n);
@@ -8,8 +11,10 @@ int _strcmp(char *s1, char *s2);
 
 void reverse_array(int *a, int n);
 
-char *string_toupper(char *_char);
+char *string_toupper(char *);
 
-char *cap_string(char *_char);
+char *cap_string(char *);
 
-char *leet(char *_char);
+char *leet(char *);
+
+#endif
