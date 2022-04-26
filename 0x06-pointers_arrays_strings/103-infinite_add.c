bf48fb841add481e296f122bb72745f8a7d624da
E-Zbox <zboxstudios97@gmail.com>
2022-04-26 01:04:35 +0100

    Check 6

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -9,22 +9,26 @@
  * Return: string literal
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
-
 {
     /* local variable declaration */
     int i = 0, j = 0, k, l = 0, f, s, d = 0;
 
     while (n1[i] != '\0')
         i++;
+
     while (n2[j] != '\0')
         j++;
+
     if (i > j)
         l = i;
     else
         l = j;
+
     if (l + 1 > size_r)
         return (0);
+
     r[l] = '\0';
+
     for (k = l - 1; k >= 0; k--)
     {
         i--;
@@ -33,6 +37,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
             f = n1[i] - '0';
         else
             f = 0;
+
         if (j >= 0)
             s = n2[j] - '0';
         else
@@ -45,8 +50,10 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
         r[l + 1] = '\0';
         if (l + 2 > size_r)
             return (0);
+
         while (l-- >= 0)
             r[l + 1] = r[l];
+
         r[0] = d + '0';
     }
     return (r);
