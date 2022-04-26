589f605d92308572d055e84936aa5f49b04f4dfc
E-Zbox <zboxstudios97@gmail.com>
2022-04-27 00:11:06 +0100

    6. Chess is mental torture

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints the chessboard
+ * @a: string literal
+ *
+ * Return: void
+ */
+void print_chessboard(char (*a)[8])
+{
+	int index1, index2;
+
+	for (index1 = 0; index1 < 8; index1++)
+	{
+		for (index2 = 0; index2 < 8; index2++)
+		{
+			_putchar(a[index1][index2]);
+		}
+		_putchar('\n');
+	}
+}
