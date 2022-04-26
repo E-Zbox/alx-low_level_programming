e5a71364e172739404561b58df832f2648575bf0
E-Zbox <zboxstudios97@gmail.com>
2022-04-26 22:13:49 +0100

    4. strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: string literal
+ * @accept: string literal
+ *
+ * Return: string literal
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int index1, index2;
+	int len_accept = 0;
+
+	char *first_occurrence = "";
+
+	while (accept[len_accept])
+		len_accept++;
+
+	index1 = index2 = 0;
+
+	for (; s[index1]; index1++)
+	{
+		for (index2 = 0; index2 < len_accept; index2++)
+		{
+			if (accept[index2] == s[index1])
+				first_occurrence = first_occurrence > (s + index1) ? (s + index1) : first_occurrence;
+		}
+	}
+
+	return (first_occurrence);
+}
+
