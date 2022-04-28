491935b4ee923a9cd4311b3707ab92ef571db36b
E-Zbox <zboxstudios97@gmail.com>
2022-04-28 02:00:24 +0100

    This should work nowgit status!

---
+++ 0x07-pointers_arrays_strings/8x-print_diagsums.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - prints the sum of the two diagonals of
+ * a square matrix of integers
+ * @a: int array pointer
+ * @size: int
+ *
+ * Return: void
+ */
+void print_diagsums(int *a, int size)
+{
+	int left_diagonal = 0;
+	int right_diagonal = 0;
+
+	int index1 = 0;
+	int index2 = 0;
+	int num;
+
+	/*
+	printf("%p\n", (void *)a);
+	printf("%p => %d\n", (void *)(a + size), *(a + size));
+
+	printf("%d\n", *(a + 3));
+	*/
+
+	for (; index1 < size; index1++)
+	{
+		/* summing up right diagonal */
+		num = *(a + ((size * (index1 + 1)) - (index1 + 1)));
+		right_diagonal += num;
+
+		for (index2 = 0; index2 < size; index2++)
+		{
+			/* summing up left diagonal */
+			if (index1 == index2)
+				left_diagonal += *(a + (index1 * size) + index2);
+		}
+	}
+
+	printf("%d, %d\n", left_diagonal, right_diagonal);
+}
