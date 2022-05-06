d4f0147ac2e34ab217346829a642479e9fcfeb5a
E-Zbox <zboxstudios97@gmail.com>
2022-05-06 02:28:15 +0100

    To infinity and beyond

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - adds positive numbers
+ * @argc: int
+ * @argv: char **
+ *
+ * Return: Always (0)
+ */
+int main(int argc, char **argv)
+{
+	int result, count, num;
+
+	result = 0;
+	count = 1;
+
+	for (; count < argc; count++)
+	{
+		num = atoi(argv[count]);
+		if (num == 0)
+		{
+			printf("Error");
+			return (1);
+		}
+
+		result += num;
+	}
+
+	printf("%d\n", result);
+
+	return (0);
+}
