ffc59fa55b6cd3528cb6f421353506e6be69ac2b
E-Zbox <zboxstudios97@gmail.com>
2022-05-09 22:27:42 +0100

    0. Trusts no one

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: unsigned int
+ *
+ * Return: void
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *pointer;
+
+	pointer = malloc(b);
+
+	if (!pointer)
+		exit (98);
+
+	return (pointer);
+}
