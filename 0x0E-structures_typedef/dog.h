a18e848e3a6078b11b3e2913ce0acd09026c9366
E-Zbox <zboxstudios97@gmail.com>
2022-05-12 17:49:39 +0100

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - define a new type struct dog
+ * @name: char *
+ * @age: float
+ * @owner: char *
+ *
+ * Description: longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void print_dog(struct dog *d);
+
+#endif
