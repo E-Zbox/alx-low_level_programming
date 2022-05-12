7aea844f322c834a97fad45dfef8d8ac935f8fda
E-Zbox <zboxstudios97@gmail.com>
2022-05-12 23:15:49 +0100

    Refactored dog.h code with main.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,23 +1,5 @@
 #ifndef DOG
 #define DOG
-
-/**
- * struct dog - define a new type struct dog
- * @name: char *
- * @age: float
- * @owner: char *
- *
- * Description: longer description
- */
-struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-};
-
-/** void print_dog(struct dog *d); */
-
-typedef struct dog dog_t;
+#include "main.h"
 
 #endif
