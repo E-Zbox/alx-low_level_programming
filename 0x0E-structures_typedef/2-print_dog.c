7bfb781e663c63f17bb79e4a5739f110e3481655
E-Zbox <zboxstudios97@gmail.com>
2022-05-12 17:49:42 +0100

    2. A dog will teach you unconditional love. If you can have that in your life, things won't be too bad

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_property - print the value passed
+ * if NULL prints (nil)
+ * @s: char *
+ *
+ * Return: void
+ */
+void print_property(char *s)
+{
+	if (s != NULL)
+		printf("%s\n", s);
+	else
+		printf("(nil)\n");
+}
+
+/**
+ * print_fproperty - print the value passed
+ * if NULL prints (nill)
+ * @s: float *
+ *
+ * Return: void
+ */
+void print_fproperty(float *d)
+{
+	if (d == NULL)
+		printf("(nil)\n");
+	else
+		printf("%f\n", *d);
+}
+
+/**
+ * print_dog - prints a struct dog
+ * @d: struct dog *
+ *
+ * Return: void
+ */
+void print_dog(struct dog *d)
+{
+	printf("Name: ");
+	print_property(d->name);
+	printf("Age: ");
+	print_fproperty(&(*d).age);
+	printf("Owner: ");
+	print_property(d->owner);
+}
