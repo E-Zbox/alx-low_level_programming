7d59f32930c8a2ad0499d22e4596b4793f73dff2
E-Zbox <zboxstudios97@gmail.com>
2022-05-25 01:33:08 +0100

    0. Print list
    Created lists header file

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * recursive_print - prints all elements of a list_t list in a recursive manner
+ * @h: list_t *
+ *
+ * Return: int
+ */
+int recursive_print(const list_t *h)
+{
+	int count = 1;
+
+	if (h->str == NULL)
+	{
+		printf("[%d] %s\n", h->len, h->str);
+		return (recursive_print(h->next) + count);
+	}
+
+	printf("[%d] %s\n", h->len, h->str);
+
+	if (h->next == NULL)
+	{
+		return (count);
+	}
+
+	return (recursive_print(h->next) + count);
+}
+
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: const list_t * (pointer to list_t)
+ *
+ * Return: int (number of nodes)
+ */
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+
+	count = recursive_print(h);
+
+	return (ount);
---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,23 @@
+#ifndef LISTS
+#define LISTS
+
+/**
+ * struct list_s - singly linked list
+ * @str: char * (malloc'ed string)
+ * @len: int (length of the string)
+ * @next: list_s * (points to the next node)
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+int _putchar(char c);
+
+size_t print_list(const list_t *h);
+
+#endif;
