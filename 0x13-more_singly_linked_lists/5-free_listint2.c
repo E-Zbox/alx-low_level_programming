18299e1d496cca572d13df4e28b90ff347010203
E-Zbox <zboxstudios97@gmail.com>
2022-05-28 12:30:46 +0100

    5. Free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,14 +8,11 @@
  */
 void free_listint2(listint_t **head)
 {
-	if (*head != NULL)
-	{
-		if ((**head).next != NULL)
-			free_listint2(&(**head).next);
+	if (head == NULL)
+		return;
+	
+	free_listint2(&(**head).next);
+	free(*head);
 
-		free((**head).next);
-		free(*head);
-
-		head = NULL;
-	}
+	head = NULL;
 }
