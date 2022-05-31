2ccd9ec08eec13f0c5669bf2314cc37044650909
E-Zbox <zboxstudios97@gmail.com>
2022-06-01 00:03:52 +0100

    4 100

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * clear_bit - sets the value of a bit to 0 at a given index
+ * @n: unsigned long int *
+ * @index: unsigned int
+ *
+ * Return: 1 if it worked, or -1 if an error occurred
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > (8 * sizeof(int) - 1))
+		return (-1);
+
+	*n = ~(1 << index) & *n;
+
+	return (1);
+}
