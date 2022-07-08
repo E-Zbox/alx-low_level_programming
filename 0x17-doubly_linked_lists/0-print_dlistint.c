3d6296fce3c439a54b1b52c246584b9770b6714b
E-Zbox <zboxstudios97@gmail.com>
2022-07-08 23:48:37 +0100

    0. Print list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,9 +10,10 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	int count;
 	const dlistint_t *node;
+	
 	node = h;
 
-	for (count=0; node != NULL; count++)
+	for (count = 0; node != NULL; count++)
 	{
 		printf("%d\n", node->n);
 		node = node->next;
